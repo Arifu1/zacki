165b0afb88d768c52af7297f8751b01d80057780
Arifu <64311996+Arifu1@users.noreply.github.com>
2021-10-04 01:18:39 +0700

    Create Kuntul.py

---
+++ Kuntul.py
@@ -0,0 +1,32 @@
+Keylogger
+
+Cara pertama yang bisa sobat lakukan untuk hack facebook orang atau gebetan adalah dengan menggunakan teknik keylogging menggunakan Keylogger.
+
+Nah disini sobat hanya perlu memasang software atau aplikasi keylogger pada PC atau laptop yang ingin di tuju (korban), yang mana keylogger ini nantinya akan merekam semua yang diketik orang yang sobat tuju, dan menjadi dalam bentuk log. Log ini akan bisa dikirimkan ke sobat baik melalui jaringan File Transfer Protocol (FTP) atau alamat email.
+
+Cara ini sudah sangat populer di kalangan orang yang ingin belajar hack. Untuk mendapatkan softwarenya, sobat bisa cari di google atau forum, karena sangat banyak bertebaran. Selamat mencoba!
+
+Menggunakan Spyic
+
+Cara lainnya yang bisa sobat coba untuk hack facebook adalah dengan menggunakan aplikasi atau software Spyic. Software ini merupakan aplikasi mata-mata yang cukup aman dan bekerja dengan baik.
+
+Spyic sendiri sudah sangat populer yang digunakan di berbagai belahan dunia, karena tampilannya yang sederhana yang membuat orang mudah menggunakannya.
+
+Dengan Spyic, sobat tidak perlu melakukan jailbreak atau root perangkat target yang ingin sobat tuju.
+
+Nah disini sobat tidak perlu meretas kata sandi atau Facebook orang lain. Dengan beberapa klik saja, sobat sudah bisa melihat pesan Facebook dan file media miliknya secara otomatis. Disamping itu sobat juga dapat melihat semua pesan masuk dan keluar. Dan juga sobat bisa melihat semuanya secara real-time. Untuk mengunduh Spyic, sobat bisa unduh dari situs resminya disini https://spyic.com/ .
+
+Meskipun Spyic sebanarnya aplikasi untuk monitoring anak ke orang tua, namun nyatanya banyak dipakai untuk hal lain. Silahkan deh sobat coba!
+
+Menggunakan Termux
+
+Termux, nama yang bukan asing lagi di kalangan para hacker awam maupun Pro. Termux adalah sebuah aplikasi yang menjalankan program-program dengan script (Codingan) yang di modifikasi. Aplikasi termux sendiri membawa fungsionalitas Linux ke dalam Android yang memungkinkan penggunanya untuk menjalankan script untuk hack seperti hack akun Facebook.
+
+Metode menggunakan termux sendiri cara yang lumayan sulit, namun ada beberapa juga yang mudah. Meskipun linux dijalankan secara teknis, sobat dapat dengan mudah menjalankan script yang dibuat oleh pengguna lain. Saya sendiri pernah menjalankan program orang lain untuk menelepon orang menggunakan nomor Piz*a Hut. Silahkan deh sobat coba, karena cara untuk menjalankan termux sangat banyak sekali!
+
+Sobat bisa menunduh termux melalui aplikasi Google Play Store untuk handphone android atau bisa klik disini.
+
+Menggunakan Spyera
+
+Cara lainnya untuk hack facebook orang lain adalah dengan menggunakan aplikasi Spyera. Spyera sendiri adalah aplikasi hack Facebook yang cukup dasar dan sederhana. Aplikasi ini memberikan keunggulan bisa digunakan di Android maupun komputer. Dengan aplikasi yang sederhana ini, sobat sudah bisa meretas akun FB orang tanpa ketahuan dengan aplikasi ini. Aplikasi ini cukup populer dan banyak digunakan karena tampilannya yang sederhana sehingga memudahkan orang. silahkan deh sobat coba! sobat bisa unduh di situs resminya disini https://spyera.com/ .
+
